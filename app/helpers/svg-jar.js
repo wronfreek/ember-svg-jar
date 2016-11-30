2a91d9258ee18e00369afef8ab377f4e7c90b484
Ron Bacalla Jr <wuffreek@Ghost.local>
2016-11-30 13:17:54 +0900

    update

--- app/helpers/svg-jar.js
+++ app/helpers/svg-jar.js
@@ -1,7 +1,9 @@
-import { htmlSafe } from 'ember-string';
+import Ember from "ember";
 import makeHelper from 'ember-svg-jar/utils/make-helper';
 import makeSVG from 'ember-svg-jar/utils/make-svg';
 import inlineAssets from '../inline-assets';
+
+const htmlSafe = Ember.String.htmlSafe;
 
 export function svgJar(assetId, svgAttrs) {
   return htmlSafe(makeSVG(assetId, svgAttrs, inlineAssets));
